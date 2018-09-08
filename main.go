393cbdc67b4c62253d8c4ab1f709e5feef4376ff
Ovidio R. Calvet <calveto@gmail.com>
2018-09-07 22:21:38 -0400

    Add implementation to handle connections

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"io"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/ocalvet/blockchain_concepts/block"
 	"github.com/ocalvet/blockchain_concepts/blockchain"
 
@@ -14,6 +20,7 @@ import (
 )
 
 var bcServer chan []block.Block
+var blkChain blockchain.Blockchain
 
 func main() {
 	// Load environment
@@ -35,7 +42,7 @@ func main() {
 	}
 
 	// Load blockchain
-	blkChain := blockchain.New(db)
+	blkChain = blockchain.New(db)
 
 	// Start web server
 	log.Fatal(server.Run(blkChain))
@@ -44,7 +51,9 @@ func main() {
 	bcServer = make(chan []block.Block)
 
 	// start TCP and serve TCP server
-	server, err := net.Listen("tcp", ":"+os.Getenv("ADDR"))
+	tcpPort := os.Getenv("NETWORK_PORT")
+	log.Printf("PORT %S", tcpPort)
+	server, err := net.Listen("tcp", ":"+tcpPort)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -60,5 +69,49 @@ func main() {
 }
 
 func handleConn(conn net.Conn) {
+	log.Println("Handling connection")
 	defer conn.Close()
+	io.WriteString(conn, "Enter a new BPM:")
+
+	scanner := bufio.NewScanner(conn)
+
+	// take in BPM from stdin and add it to blockchain after conducting necessary validation
+	go func() {
+		for scanner.Scan() {
+			bpm, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%v not a number: %v", scanner.Text(), err)
+				continue
+			}
+			blks := blkChain.Get()
+			newBlock, err := block.NewBlock(blks[len(blks)-1], bpm)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if newBlock.IsValid(blks[len(blks)-1]) {
+				newBlockchain := append(blks, newBlock)
+				blkChain.Replace(newBlockchain)
+			}
+
+			bcServer <- blkChain.Get()
+			io.WriteString(conn, "\nEnter a new BPM:")
+		}
+	}()
+
+	// simulate receiving broadcast
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			output, err := json.Marshal(blkChain.Get())
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.WriteString(conn, string(output))
+		}
+	}()
+
+	for _ = range bcServer {
+		spew.Dump(blkChain.Get())
+	}
 }
